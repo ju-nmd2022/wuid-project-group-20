9c056d561d51bd39fb687cc2e78c31201a42eead
NataliaFedakova <117270929+NataliaFedakova@users.noreply.github.com>
2023-02-06 17:55:04 +0100

    Update foodmobile.js

--- foodmobile.js
+++ foodmobile.js
@@ -1,12 +1,4 @@
-<!-- The following 17 lines of code was adapted from https://www.youtube.com/watch?v=OFKBep95lb4&list=LL&index=2 -->
-window.onload = function () {
-	window.addEventListener('scroll', function (e) {
-		if (window.pageYOffset > 100) {
-			document.querySelector("header").classList.add('is-scrolling');
-		} else {
-			document.querySelector("header").classList.remove('is-scrolling');
-		}
-	});
+<!-- The following 9 lines of code was adapted from https://www.youtube.com/watch?v=OFKBep95lb4&list=LL&index=2 -->
 
 	const menu_btn = document.querySelector('.hamburger');
 	const mobile_menu = document.querySelector('.mobile-nav');
