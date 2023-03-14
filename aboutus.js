7e33eb1c4ad0e2a6ba799f3bd83815c6c2c36571
gurkan-lite <117160581+gurkan-lite@users.noreply.github.com>
2023-03-14 19:58:17 +0100

    js css update

--- aboutus.js
+++ aboutus.js
@@ -1,6 +1,11 @@
-const emojiBtn = document.querySelector("#emoji");
-const picker = new EmojiButton();
+const popup = document.querySelector(".chat-popup");
+const chatBtn = document.querySelector(".chat-btn");
+const submitBtn = document.querySelector(".submit");
+const chatArea = document.querySelector(".chat-area");
+const inputElm = document.querySelector("input");
+const emojiBtn = document.querySelector("#emoji-btn");
 
+// Emoji
 window.addEventListener("DOMContentLoaded", () => {
   picker.on("emoji", (emoji) => {
     document.querySelector("input").value += emoji;
@@ -9,4 +14,23 @@ window.addEventListener("DOMContentLoaded", () => {
   emojiBtn.addEventListener("click", () => {
     picker.togglePicker(emojiBtn);
   });
+});
+
+// Chat btn toggel
+
+chatBtn.addEventListener("click", () => {
+  popup.classList.toggle("show");
+});
+
+// send msg
+submitBtn.addEventListener("click", () => {
+  let userInput = inputElm.value;
+
+  let temp = `<div class="out-msg">
+    <span class="my-msg">${userInput}</span>
+    <img src="img/chat.jpg" class="avatar">
+    </div>`;
+
+  chatArea.insertAdjacentHTML("beforeend", temp);
+  inputElm.value = "";
 });
