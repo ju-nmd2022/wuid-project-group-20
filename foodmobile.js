ee47fcf240a8d5d7bef881d5be45a4be04b2a434
gurkan-lite <117160581+gurkan-lite@users.noreply.github.com>
2023-03-17 10:41:37 +0100

    Update

--- foodmobile.js
+++ foodmobile.js
@@ -1,9 +1,9 @@
-<!-- The following 9 lines of code was adapted from https://www.youtube.com/watch?v=OFKBep95lb4&list=LL&index=2 -->
+// <!-- The following 9 lines of code was adapted from https://www.youtube.com/watch?v=OFKBep95lb4&list=LL&index=2 -->
 
-	const menu_btn = document.querySelector('.hamburger');
-	const mobile_menu = document.querySelector('.mobile-nav');
+const menu_btn = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
 
-	menu_btn.addEventListener('click', function () {
-		menu_btn.classList.toggle('is-active');
-		mobile_menu.classList.toggle('is-active');
-	});
+menu_btn.addEventListener("click", function () {
+  menu_btn.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
+});
